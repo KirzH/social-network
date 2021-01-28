b53fee804f5f07e9d6547fac1f38e305ced38fec
KirzH <kirzhakov.2012@mail.ru>
2021-01-28 11:37:03 +0300

    fix /profile

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -6,11 +6,21 @@ const Navbar = () => {
     return (
        <nav className={a.nav}>
          <div className={a.item}>
-          <div><a>Profile</a></div>
-          <div><a>Messages</a></div>
-          <div><a>News</a></div>
-          <div><a>Music</a></div>
-          <div><a>Settings</a></div>
+          <div>
+            <a href="/profile" >Profile</a>
+          </div>
+          <div>
+            <a>Messages</a>
+          </div>
+          <div>
+            <a>News</a>
+          </div>
+          <div>
+            <a>Music</a>
+          </div>
+          <div>
+            <a>Settings</a>
+          </div>
          </div>
       </nav>
     );

4d352441243ff8dd0ecf23ef5eea9a21e7e408ae
Vadim Kirzhakov <kirzhakov.2012@mail.ru>
2021-03-04 23:34:16 +0300

    fix axios

--- src/components/users/users.js
+++ src/components/users/users.js
@@ -1,14 +1,27 @@
+import * as axios from "axios"
 import React from "react"
 import styles from "./users-module.css"
+import userPhoto from "../../assets/img/userPhoto.png"
 
 const Users = props => {
+	if (props.users.lenght === 0) {
+		axios
+			.get("https://social-network.samuraijs.com/api/1.0/users")
+			.then(response => {
+				props.setUsers(response.data.items)
+			})
+	}
+
 	return (
 		<div>
 			{props.users.map(u => (
 				<div key={u.id}>
 					<span>
 						<div>
-							<img src={u.photoUrl} className={styles.userPhoto} />
+							<img
+								src={u.photos.small != null ? u.photos.small : userPhoto}
+								className={styles.userPhoto}
+							/>
 						</div>
 						<div>
 							{u.followed ? (
@@ -32,12 +45,12 @@ const Users = props => {
 					</span>
 					<span>
 						<span>
-							<div>{u.fullName}</div>
+							<div>{u.name}</div>
 							<div>{u.status}</div>
 						</span>
 						<span>
-							<div>{u.location.country}</div>
-							<div>{u.location.city}</div>
+							{/* <div>{u.location.country}</div> */}
+							{/* <div>{u.location.city}</div> */}
 						</span>
 					</span>
 				</div>
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -3,35 +3,7 @@ const UNFOLLOW = "UNFOLLOW"
 const SET_USERS = "SET_USERS"
 
 let initialState = {
-	users: [
-		{
-			id: 1,
-			photoUrl:
-				"https://sun1-96.userapi.com/s/v1/ig2/enwfpnTYksgZlxmoRBQ_kyUYUQcb6GE3JjKYPdGKDW9MtLrbbd5Pbp-HTZSeIEilPyYUqum9YzjeoDHtTkPq_LFA.jpg?size=200x0&quality=96&crop=1,271,1615,1615&ava=1",
-			followed: true,
-			fullName: "Mikhail",
-			status: "I am your best friend",
-			location: { city: "Kalinkovichi", country: "Belarus" },
-		},
-		{
-			id: 2,
-			photoUrl:
-				"https://sun1-91.userapi.com/s/v1/if1/YFwf_zJH9ZMJC7HILvb9wooWoVtSx_df97oLb9RqtpMbpXj_JC3h4y5aT_Kad8ABXvoIFvAz.jpg?size=200x0&quality=96&crop=181,0,1430,1430&ava=1",
-			followed: true,
-			fullName: "Artyom",
-			status: "I am your best friend",
-			location: { city: "Gorodey", country: "Belarus" },
-		},
-		{
-			id: 3,
-			photoUrl:
-				"https://sun1-21.userapi.com/s/v1/ig2/JgyCKsstVa-aQzPrubF0pf_UL_2cW5jOl1ctP5hwiq-IT72tmugI77MGIcME6SfLCFe2lTIMkz--zc9dnWZQXx2b.jpg?size=100x0&quality=96&crop=0,92,736,736&ava=1",
-			fullName: "Rodion",
-			followed: false,
-			status: "I am your best friend",
-			location: { city: "Balashikha", country: "Russia" },
-		},
-	],
+	
 }
 
 export const usersReducer = (state = initialState, action) => {
